9560e89efb9bcccf9ba3a460c321044a5ca27d68
jocha college <gregorykimanthi100@gmail.com>
2025-01-13 23:05:51 +0300

    do more styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { useState } from "react";
+// import { useState } from "react";
+// import background from "./assets/ave maria.jpg";
 import "./index.css";
 import InputBox from "./components/input";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <InputBox />
+    <div className="main-content">
+      <InputBox />
+    </div>
   </StrictMode>
 );
