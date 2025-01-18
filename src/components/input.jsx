891f752dd93939925034d0baed516f2545f20eb6
JOCHA COLLEGE <gregorykimanthi100@gmail.com>
2025-01-18 11:28:57 +0300

    update package.json for deployement

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -27,7 +27,7 @@ export default function InputBox() {
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await fetch("/data.json");
+        const response = await fetch(`${import.meta.env.BASE_URL}data.json`);
         if (!response.ok) throw new Error("Failed to fetch songs");
         const json = await response.json();
         setData(json);
